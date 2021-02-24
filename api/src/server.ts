98d44b2de470a6821fee727e1523b0c997bd2ad2
zepanzeri <zepanzeri94@gmail.com>
2021-02-24 11:31:07 -0300

    Configurations of typeorm, created migrations, user model, user controller and user router.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,16 +1,13 @@
 import "reflect-metadata";
-import express, { request, response } from "express";
+import express from "express";
+
 import "./database";
+import { router } from "./routes";
 
 
 const app = express();
 
-app.get("/",(request, response) =>{
-    return response.json({message:"Hello NLW4"});
-});
+app.use(express.json());
+app.use(router);
 
-app.post("/", (request, response) =>{
-    return response.json({message:"Data saved successfully"});
-});
-
-app.listen(3333,()=>console.log("Server is running"));
+app.listen(3333, ()=> console.log("Server is running"));
