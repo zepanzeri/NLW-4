ca82e8c4c119dcee9ba2f785145ce94fc09193f2
zepanzeri <zepanzeri94@gmail.com>
2021-02-25 09:17:30 -0300

    Refactored the controller, created user respository, changed the controller to use the created repository.

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { getCustomRepository } from "typeorm";
 import { User } from "../models/User";
+import { UsersRepository } from "../repositories/UserRepositories";
 
 export class UserController{
     async create(request:Request, response:Response){
         const {name, email} = request.body;
-        const userRepository = getRepository(User)
+        const userRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExist = await userRepository.findOne({
             email
